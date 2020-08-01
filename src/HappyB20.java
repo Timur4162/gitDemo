f44c4fcea3e703865ee914d75b172bd79b5b8454
Timur4162 <timur.izbossinov@gmail.com>
2020-08-01 09:56:36 -0600

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,5 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy!");
+
+        System.out.println("I learn java language in the Cybertek School");
     }
 }
