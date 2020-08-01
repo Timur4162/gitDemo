2c38dd1db1ed739ba6caa33b78500c076c00e557
Timur4162 <timur.izbossinov@gmail.com>
2020-08-01 12:30:41 -0600

    updated newClassDemo.java

--- src/newClassDemo.java
+++ src/newClassDemo.java
@@ -2,5 +2,8 @@ public class newClassDemo {
     public static void main(String[] args) {
 
         System.out.println("Demo");
+        System.out.println("Demo2");
+
+        System.out.println("========");
     }
 }
